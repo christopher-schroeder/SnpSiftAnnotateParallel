4085edcba8eeae553712c9f2a8d863ecd85263da
Christopher Schröder <christopher.schroeder@tu-dortmund.de>
2020-03-19 11:00:33 +0100

    script

---
+++ ssap.py
@@ -0,0 +1,46 @@
+import os
+import argparse
+from multiprocessing import Pool
+from pysam import VariantFile
+import tempfile
+
+parser = argparse.ArgumentParser()
+parser.add_argument("database_vcf")
+parser.add_argument("file_vcf")
+parser.add_argument("--stepsize", "-s", type=int, default=1000000)
+parser.add_argument("--threads", "-t", type=int, default=8)
+parser.add_argument("--tmp", default=None, help="Define the base directory for the tmp files")
+args, unknown = parser.parse_known_args()
+
+ss_args = " ".join(unknown)
+
+tmp_base = args.tmp
+stepsize = args.stepsize
+
+# create regions
+f = VariantFile(args.file_vcf)
+regions = []
+for name, contig in f.header.contigs.items():
+    last = 0
+    for x in list(range(0, contig.length, stepsize)):
+        stop = min(last + stepsize - 1, contig.length)
+        regions.append(f"{name}:{last}-{stop}")
+        last = stop+1
+f.close() 
+
+with tempfile.TemporaryDirectory(dir=tmp_base) as t:
+    def f(x):
+        cmd = f"bcftools view -r {x} {args.file_vcf} | bcftools view -t {x} | SnpSift annotate {ss_args} {args.database_vcf} /dev/stdin | bcftools view -Ob > {t}/{x}.bcf"
+        print(cmd)
+        os.system(cmd)
+        return f"{t}/{x}.bcf"
+
+    with Pool(args.threads) as p:
+        first = True
+        for filename in p.imap(f, regions):
+            if first:
+                cmd = f"bcftools view {filename}"
+            else:
+                cmd = f'bcftools view {filename} | grep -v "#"'
+            os.system(cmd)
+            first = False
